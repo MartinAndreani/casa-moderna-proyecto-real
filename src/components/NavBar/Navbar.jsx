f8bb3fc158065a1564de179f9c12fed383136440
MartinAndreani <andreani.martin4@gmail.com>
2024-03-13 15:15:44 -0300

    nav and photos

---
+++ src/components/NavBar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div className='bg-nav h-24'>
+            Casa Moderna
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "nav": "#242021",
+      }
+    },
   },
   plugins: [],
 }
