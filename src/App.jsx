240560214e2b11cbe3868e0b1f25c8e96d1b4a63
AllanNara <allannara@outlook.com>
2024-03-14 18:21:19 -0300

    add 'servicios' to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './components/NavBar/Navbar'
+import Servicios from './components/Servicios/Servicios'
 
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <Servicios/>
     </div>
   )
 }
