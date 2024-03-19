ca9c2476344b27e6784129fcb569a818a6719003
MartinAndreani <andreani.martin4@gmail.com>
2024-03-19 00:35:23 -0300

    nav y footer desktop, instalacion react icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Navbar from './components/NavBar/Navbar'
+import Footer from './components/Footer/Footer'
 
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <div>
+        .
+      </div>
+      <Footer/>
     </div>
   )
 }
--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -8,21 +8,22 @@ const Navbar = () => {
         <div className='bg-nav h-28 flex items-center justify-around  w-full  '>
 
             {/* logo */}
-            <div className='flex justify-center'>
+            <div className='flex justify-center transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110  duration-300  '>
               <a href="/">
-                <img src={logo} alt="/" className='h-24 '/>
+                <img src={logo} alt="/" className='h-24  '/>
               </a>
               
             </div>
 
 
             {/* Opciones */}
-            <div className=' relative left-16'>
-              <ul className='flex gap-x-8 justify-center items-center'>
-                <li className='flex justify-center items-center w-28 text-white '><a href="/">Sobre Nosotros</a></li>
-                <li className='flex justify-center w-28 text-white '><a href="/">Sistemas</a></li>
-                <li className='flex justify-center w-28 text-white '><a href="/">Servicios</a></li>
-                <li className='flex justify-center w-28 text-white '><a href="/">Contactos</a></li>
+            <div className=' relative left-12'>
+              <ul className='flex gap-5 justify-center items-center'>
+                <li className='flex justify-center items-center w-28  text-white transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110  duration-300 '><a href="/">Sobre Nosotros</a></li>
+                <li className='flex justify-center w-28 text-white transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110  duration-300'><a href="/">Sistemas</a></li>
+                <li className='flex justify-center w-28 text-white transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110  duration-300'><a href="/">Servicios</a></li>
+                <li className='flex justify-center w-28 text-white transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110  duration-300'><a href="/">Contactos</a></li>
+                <li className='flex justify-center w-28 text-white transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110  duration-300'><a href="/">Galería</a></li>
 
               </ul>
               
@@ -30,9 +31,9 @@ const Navbar = () => {
 
 
             {/* BOTON  */}
-            <div className='bg-botones flex justify-start items-center relative right-12  rounded-2xl text-white '>
+            <div className='bg-botones flex justify-start items-center relative right-12  rounded-2xl text-white transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110  duration-300  '>
               <button className='px-5 py-3'>Solicitar cotización</button>
-            </div>
+            </div> 
 
         </div>
 
