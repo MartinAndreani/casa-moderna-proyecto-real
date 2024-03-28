6a51463ea3dc7df69fc57368c4592afa1e09037c
AllanNara <allannara@outlook.com>
2024-03-28 18:48:34 -0300

    about us: finished first section

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import BannerHome from './components/Home/BannerHome/BannerHome' 
-import Navbar from './components/NavBar/Navbar' 
-import Servicios from './components/Servicios/Servicios'
-import Footer from './components/Footer/Footer'
-import SobreNosotros from './components/SobreNosotros/SobreNosotros'
-
+import BannerHome from "./components/Home/BannerHome/BannerHome";
+import Navbar from "./components/NavBar/Navbar";
+import Servicios from "./components/Servicios/Servicios";
+import Footer from "./components/Footer/Footer";
+import SobreNosotros from "./components/SobreNosotros/SobreNosotros";
 
 const App = () => {
-  return (
-    <div className="bg-gradient-to-b from-white via-gray-200 to-gray-400 h-auto font-roboto ">
-      <Navbar/>
-      <BannerHome/>
-      <Servicios/>
-      <SobreNosotros/>
-      <Footer/>
-    </div>
-  )
-}
+	return (
+		<div className="font-roboto">
+			<Navbar />
+			<div className="bg-gradient-to-b from-white via-gray-200 to-gray-300 h-auto">
+				<BannerHome />
+				<Servicios />
+			</div>
+			<SobreNosotros />
+			<Footer />
+		</div>
+	);
+};
 
-export default App
+export default App;
