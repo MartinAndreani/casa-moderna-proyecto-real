29fa5137829d81caaf55a6245ffab5082f879e89
MartinAndreani <andreani.martin4@gmail.com>
2024-04-14 00:26:52 -0300

    arreglo home

--- src/components/Home/BannerHome/BannerHome.jsx
+++ src/components/Home/BannerHome/BannerHome.jsx
@@ -2,9 +2,9 @@ import ButtonGetInfo from "../../common/ButtonGetInfo/ButtonGetInfo";
 
 const BannerHome = () => {
 	return (
-		<section className="flex flex-row lg:p-24 gap-3 lg:gap-14 ml-5 lg:ml-[5rem]" id="home">
-			<div className="flex flex-col justify-center gap-10 lg:basis-1/2 w-80  text-left lg:pt-14">
-				<p className="text-gray-500 text-xl -my-5">Casa Moderna</p>
+		<section className="flex flex-row  gap-3 lg:gap-14 ml-5 lg:ml-[5rem] pt-32 py-10 pr-5 " id="home">
+			<div className="flex flex-col justify-center gap-10 lg:basis-1/2 w-96  text-left lg:pt-14">
+				<p className="text-gray-500 text-xl ">Casa Moderna</p>
 				<div className="flex flex-col gap-10">
 					<h1 className="lg:text-5xl text-4xl  font-extrabold text-gray-600">
 						Construcciones y ampliaciones industrializadas de excelencia
@@ -20,7 +20,7 @@ const BannerHome = () => {
 					</a>
 				</div>
 			</div>
-			<div className="pt-1.5 bg-[url('images/home.png')]  h-[31rem] w-64 lg:w-[28rem] rounded-t-[12.5rem] shadow-2xl shadow-black bg-cover lg:mt-16"></div>
+			<div className="pt-1.5 bg-[url('images/home.png')]  h-[31rem] w-96 lg:w-[28rem] rounded-t-[12.5rem] shadow-2xl shadow-black bg-cover lg:mt-16"></div>
 		</section>
 	);
 };
